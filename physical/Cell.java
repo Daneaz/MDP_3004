f97f8838864819e85354b344d2e51d123a059279
Jlian003 <jlian003@e.ntu.edu.sg>
2018-02-23 09:33:59 +0800

    Add files via upload

---
+++ physical/Cell.java
@@ -0,0 +1,79 @@
+package model.physical;
+
+public class Cell implements Comparable<Cell> {
+
+	private int x;
+	private int y;
+	private int distance;
+	private boolean explored;
+	private boolean isObstacle;
+	private int count = 0;
+	
+	Cell() {}
+	
+	public Cell (int x, int y){
+		
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX(){
+		return this.x;
+	}
+	
+	public int getY(){
+		return this.y;
+	}
+	
+	private int getDistance(){
+		return this.distance;
+	}
+	
+	public void getDistance(int distance){
+		this.distance = distance;
+	}
+	
+	public boolean getExplored(){
+		return this.explored;
+	}
+	
+	 void setExplored(boolean explored){
+		this.explored = explored;
+	}
+	
+	public boolean getIsObstacle(){
+		return this.isObstacle;
+	}
+	
+	void setIsObstacle(boolean isObstacle){
+		this.isObstacle = isObstacle;
+	}
+	
+	void updateCount(int num){
+		count += num;
+		isObstacle = count > 0;
+	}
+	
+	@Override
+	public int compareTo(Cell o) {
+		// TODO Auto-generated method stub
+		if(this.distance > o.getDistance())
+			return 1;
+		else if (this.distance < o.getDistance())
+			return -1;
+		else 
+			return 0;
+	}
+	
+	@Override
+	public boolean equals(Object o){
+		if(o instanceof Cell){
+			Cell otherCell = (Cell)o;
+			if(otherCell.getY() == getY() && otherCell.getX() == getX())
+				return true;
+		}
+		
+		return false;
+	}
+
+}
